2dd5d0cdd6e39e9c284a5e64a6dca6b707e7d441
7Davronbek <maxmudovdavronbek8@gmail.com>
2023-03-16 20:50:58 +0300

    admin-dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import { HashRouter, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Layout from "./components/Layout";
+
 const App = () => {
-  return <span>Subscribe Zain keepscode</span>;
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="dashboard" element={<Dashboard />} />
+        </Route>
+      </Routes>
+    </HashRouter>
+  );
 };
 
 export default App;
---
+++ src/components/Layout/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./layout.scss";
+
+const Layout = () => {
+  return <div className="Layout"></div>;
+};
+
+export default Layout;
