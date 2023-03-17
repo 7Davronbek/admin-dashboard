12718823f533432742b67717f4c111d72f79bcb4
7Davronbek <maxmudovdavronbek8@gmail.com>
2023-03-17 22:43:57 +0300

    admin-dashboard

--- src/pages/Board/index.jsx
+++ src/pages/Board/index.jsx
@@ -5,13 +5,34 @@ import Board, {
   removeCard,
   addCard,
 } from "@asseinfo/react-kanban";
+import useBoard from "../../store/Board";
 
 const BoardPage = () => {
+  const { board, setBoard } = useBoard();
+
+  const handleColumnMove = (_card, source, destination) => {
+    const updateBoard = moveColumn(board, source, destination)
+    setBoard(updateBoard)
+  }
+
+  const handleCardMove = (_card, source, destination) => {
+    const updateBoard = moveCard(board, source, destination)
+    setBoard(updateBoard)
+  }
+
   return (
-    <div className="BoardPage">
+    <div className="BoardPage py-5">
       <div className="container">
         <div className="row">
-          <div className="col-12"></div>
+          <div className="col-12">
+            <Board 
+             allowAddColumn
+             allowRenameColumn
+             allowRemoveCard
+             onCardDragEnd={handleCardMove}
+             onColumnDragEnd={handleColumnMove}
+            >{board}</Board>
+          </div>
         </div>
       </div>
     </div>
